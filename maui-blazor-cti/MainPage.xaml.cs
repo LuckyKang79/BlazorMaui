01820062dd7aefbf478124285d6c58ca8f7ebe84
Lucky kang (CSI Interfusion Inc) <v-wakang@microsoft.com>
2022-06-24 13:58:24 +0800

    add solution

---
+++ maui-blazor-cti/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace maui_blazor_cti;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ maui-blazor-cti/obj/Debug/net6.0-android/android/src/crc64882605a4d77270e1/MainActivity.java
@@ -0,0 +1,46 @@
+package crc64882605a4d77270e1;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("maui_blazor_cti.MainActivity, maui-blazor-cti", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("maui_blazor_cti.MainActivity, maui-blazor-cti", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("maui_blazor_cti.MainActivity, maui-blazor-cti", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
